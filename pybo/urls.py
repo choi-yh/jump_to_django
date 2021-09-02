5309e8f2bc8a1438b1b9a4446c84fa106376d116
younghyo_office <yhchoi0102@gmail.com>
2021-09-02 18:52:27 +0900

    2-05 URL과 네임스페이스

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "pybo" # url namespace
+
 urlpatterns = [
-    path("", views.index),
-    path("<int:question_id>/", views.detail),
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),  # url 별칭 지정
 ]
