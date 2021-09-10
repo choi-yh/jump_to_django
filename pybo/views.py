ee75f61bdc37c4b47bd1561c1e049ebd1560e319
younghyo_desktop <yhchoi0102@gmail.com>
2021-09-10 22:20:53 +0900

    2-06 데이터 저장

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render, get_object_or_404  # render : 파이썬 데이터를 템플릿에 적용해 HTML로 반환하는 함수
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect,
+)  # render : 파이썬 데이터를 템플릿에 적용해 HTML로 반환하는 함수
+from django.utils import timezone
 from .models import Question
 
 
@@ -21,3 +26,12 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {"question": question}
     return render(request, "pybo/question_detail.html", context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect("pybo:detail", question_id=question.id)
